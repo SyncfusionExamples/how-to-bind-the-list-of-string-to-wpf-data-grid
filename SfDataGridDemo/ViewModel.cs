307457591131fe7f2b2f4952d54c0ee9c6a64294
FarjanaParveen <farjanaparveena@syncfusion.com>
2020-04-06 13:16:11 +0530

    How to bind the list of string to WPF DataGrid(SfDataGrid)?

---
+++ SfDataGridDemo/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_MVVM
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.DataFieldList = new List<string>() { "Item1", "Item2" , "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10" };
+        }
+
+        public List<string> DataFieldList
+        {
+            get;
+            set;
+        }
+    }
+}
